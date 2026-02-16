30b492afa00b323c78a78e9d20fbd4006ebfb8e9
Jackson-P-Peterson <Jackson-P-Peterson@users.noreply.github.com>
2026-02-16 05:49:49 +0000

    deploy: 81cc80be3c0dde68384c954d6fb65af38e5581c3

---
+++ _next/static/qRj3rn6WNg7NV8PY_r1Fh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/carmen-montano-senate-website//_next/:path+",
+        "destination": "/carmen-montano-senate-website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
